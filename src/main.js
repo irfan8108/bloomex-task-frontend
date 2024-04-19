690a04893a64ab15edbd46bc261a3c16bb02a49d
ALI <irfan8108@gmail.com>
2024-04-20 03:42:34 +0530

    ssl protocol fixed

--- src/main.js
+++ src/main.js
@@ -5,9 +5,7 @@ import "bootstrap/dist/css/bootstrap.css"
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
 
-// We are not using Axios Base URL - Deprecated since Vue CLI 3.3
-// We achived such requirement via Proxy Server also solved the Cors Issue
 import axios from 'axios'
-axios.defaults.baseURL = "http://54.253.70.47:8000/public/api/"
+axios.defaults.baseURL = "https://54.253.70.47:8000/public/api/"
 
 createApp(App).mount('#app')
